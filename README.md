ad510af97e205c3258f938d3c9ef863074967465
Rossi <rosalcis@gmail.com>
2021-05-06 17:16:32 +0200

    Missatge commit intento subir un commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # prueba1
 intento descubrir las maravillas de GIT
+y además tener un espacio para subir la prueba
+por ahora...va bien :-D
---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<!doctype html>
+<html lang="es">
+<head>
+	<!-- Informació Meta -->
+	<meta charset="utf-8"/>
+	<meta name="description" content="Lorem Ipsum">
+	<meta name="keywords" content="Lorem, Ipsum">
+	<meta name="author" content="Lorem Ipsum">
+	
+	<!-- Enllaç a l'arxiu CSS Extern -->
+	<link rel=stylesheet href="css/style.css" type="text/css"/>
+	
+	<!--CSS Intern-->
+	<style type="text/css">
+		
+	</style>
+	
+	<!-- Enllaç a Javascript Extern -->
+	<script  type="text/javascript" src="js/javascript.js"></script>
+	
+	<!-- favicon -->
+	<link href="img/favicon.png" rel="icon" type="image/png" />
+	
+	<!-- Títol de la pàgina -->
+	<title>Títol de la pàgina</title>
+</head>
+<body>
+	<header></header>
+	<section>
+		<article>
+			<h2>Lorem Ipsum</h2>
+			<?php echo "Hola Món!" ?>
+		</article>
+	</section>
+	<footer></footer>
+</body>
+</html>
